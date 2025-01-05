1417a255da464cae8584af6f4f50da484264566d
ffayad <ffayad@ptcdb.edu.ps>
2025-01-05 13:09:38 +0200

    program to read first n lines of a file

--- question_2.py
+++ question_2.py
@@ -1,1 +1,10 @@
 #2- Write a Python program to read first n lines of a file
+i  = 0
+f = open('C:/Users/Fatoom/Documents/names.txt')
+lines = f.readlines()
+names = []
+for line in lines:
+    if i <= 3:
+        names.append(lines[i].strip())
+        i+=1
+print(names)
