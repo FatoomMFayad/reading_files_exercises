4bee6963f82637b304c7dd4a415c7e7bb9b46d2b
ffayad <ffayad@ptcdb.edu.ps>
2025-01-06 10:09:45 +0200

    using os join function

--- list_directory.py
+++ list_directory.py
@@ -1,6 +1,6 @@
 import os
 files_list = os.listdir('C:/Users/Fatoom/Documents/text_files')
 for file in files_list:
-   f = open('C:/Users/Fatoom/Documents/text_files/' + file)
+   f = open(os.path.join('C:/Users/Fatoom/Documents/text_files' , file))
    numbers = [int(number) for number in f]
    print(sum(numbers))
