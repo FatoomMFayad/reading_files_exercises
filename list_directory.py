f65830a929753cc5e72d5c70ac770e6dc108c649
ffayad <ffayad@ptcdb.edu.ps>
2025-01-06 10:07:57 +0200

    sum using list comprehension

--- list_directory.py
+++ list_directory.py
@@ -2,6 +2,5 @@ import os
 files_list = os.listdir('C:/Users/Fatoom/Documents/text_files')
 for file in files_list:
    f = open('C:/Users/Fatoom/Documents/text_files/' + file)
-   numbers = f.readlines()
-   numbers2 = [int(number) for number in numbers]
-   print(sum(numbers2))
+   numbers = [int(number) for number in f]
+   print(sum(numbers))
