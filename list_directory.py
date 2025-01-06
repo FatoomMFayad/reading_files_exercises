1f7ecc485ab90a5b69b1dd246823f8f089d51127
ffayad <ffayad@ptcdb.edu.ps>
2025-01-06 10:13:31 +0200

    add to dictionary

--- list_directory.py
+++ list_directory.py
@@ -1,7 +1,9 @@
 import os
 directory_path = 'C:/Users/Fatoom/Documents/text_files'
 files_list = os.listdir(directory_path)
+files_sum_dic = {}
 for file in files_list:
    f = open(os.path.join(directory_path , file))
    numbers = [int(number) for number in f]
-   print(sum(numbers))
+   files_sum_dic.update({file: sum(numbers)})
+print(files_sum_dic)
